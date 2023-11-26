131bcd13ba778bfe0182be7924227e4c12b4ad42
niracler <i@niracler.com>
2023-11-26 16:07:02 +0800

    feat: sync message from telegram to twitter

---
+++ src/telegram.ts
@@ -0,0 +1,76 @@
+// telegram.js
+import * as twitter from './twitter'
+import { Env, TelegramUpdate } from './type'
+
+// Process the Telegram update received
+export async function handleTelegramUpdate(update: TelegramUpdate, env: Env) {
+    const allowedUserIds = env.ALLOW_USER_IDS
+    const fromUserId = update.message?.from?.id.toString() || ''
+    const fromUsername = update.message?.from?.username || ''
+
+    // Exit if there is no message text
+    if (!update.message?.text) return
+    let replyText = ''
+
+    // Check if the user is allowed to interact with the bot
+    if (!allowedUserIds.includes(fromUsername) && !allowedUserIds.includes(fromUserId)) {
+        return
+    } else if (update.message.text.startsWith('/sync_twitter')) {
+        replyText = await processSyncTwitterCommand(update, env)
+    } else {
+        replyText = `Echo: ${update.message.text}`
+    }
+
+    // Send a reply message to Telegram
+    await sendReplyToTelegram(update.message.chat.id, replyText, update.message.message_id, env)
+}
+
+// Process the '/sync_twitter' command
+async function processSyncTwitterCommand(update: TelegramUpdate, env: Env): Promise<string> {
+    if (!update.message?.reply_to_message?.photo?.length) {
+        return 'No photo found to sync with Twitter.'
+    }
+
+    const bestPhoto = update.message.reply_to_message.photo[update.message.reply_to_message.photo.length - 1]
+    const photoUrl = await getTelegramFileUrl(bestPhoto.file_id, env.TELEGRAM_BOT_SECRET)
+    const mediaData = await fetch(photoUrl).then(res => res.arrayBuffer())
+
+    try {
+        const media = await twitter.uploadMediaToTwitter(mediaData, env)
+        const tweetContent = `${update.message.reply_to_message.caption} #sync_from_telegram`
+        const tweet = await twitter.postTweet(tweetContent, [media.media_id_string], env)
+
+        return `Your message has been posted to Twitter. Id: ${tweet.data.id}`
+    } catch (error) {
+        return `Failed to post tweet: ${error}`
+    }
+}
+
+
+// Fetch the file URL from Telegram using the file_id
+async function getTelegramFileUrl(fileId: string, botSecret: string): Promise<string> {
+    const fileResponse = await fetch(`https://api.telegram.org/bot${botSecret}/getFile?file_id=${fileId}`)
+    if (!fileResponse.ok) {
+        throw new Error(`Telegram API getFile responded with status ${fileResponse.status}`)
+    }
+    const fileData = await fileResponse.json() as { ok: boolean, result: { file_path: string } }
+    const filePath = fileData.result.file_path
+    return `https://api.telegram.org/file/bot${botSecret}/${filePath}`
+}
+
+// Send a message back to Telegram chat
+async function sendReplyToTelegram(chatId: number, text: string, messageId: number, env: Env) {
+    const response = await fetch(`https://api.telegram.org/bot${env.TELEGRAM_BOT_SECRET}/sendMessage`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            chat_id: chatId,
+            text,
+            reply_to_message_id: messageId,
+        }),
+    })
+
+    if (!response.ok) {
+        throw new Error(`Telegram API sendMessage responded with status ${response.status}`)
+    }
+}
---
+++ src/twitter.ts
@@ -0,0 +1,70 @@
+import OAuth from 'oauth-1.0a'
+import { HmacSHA1, enc } from 'crypto-js'
+import { Buffer } from 'node:buffer'
+import { Env } from "./type"
+
+export async function uploadMediaToTwitter(mediaData: ArrayBuffer, env: Env): Promise<any> {
+    const oauth = new OAuth({
+        consumer: { key: env.TWITTER_API_KEY, secret: env.TWITTER_API_SECRET },
+        signature_method: 'HMAC-SHA1',
+        hash_function(baseString, key) {
+            return HmacSHA1(baseString, key).toString(enc.Base64)
+        },
+    })
+
+    const oauthToken = {
+        key: env.TWITTER_ACCESS_TOKEN,
+        secret: env.TWITTER_ACCESS_TOKEN_SECRET,
+    }
+
+    const requestData = {
+        url: 'https://upload.twitter.com/1.1/media/upload.json?media_category=tweet_image',
+        method: 'POST',
+        data: {
+            media_data: Buffer.from(mediaData).toString('base64'),
+        },
+    }
+
+    // 初始化媒体上传以获取media_id
+    const response = await fetch(requestData.url, {
+        method: 'POST',
+        headers: {
+            ...oauth.toHeader(oauth.authorize(requestData, oauthToken)),
+            'content-type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({ media_data: Buffer.from(mediaData).toString('base64') }),
+    })
+
+    return await response.json()
+}
+
+export async function postTweet(text: string, mediaList: string[], env: Env): Promise<any> {
+    const oauth = new OAuth({
+        consumer: { key: env.TWITTER_API_KEY, secret: env.TWITTER_API_SECRET },
+        signature_method: 'HMAC-SHA1',
+        hash_function(baseString, key) {
+            return HmacSHA1(baseString, key).toString(enc.Base64)
+        },
+    })
+
+    const oauthToken = {
+        key: env.TWITTER_ACCESS_TOKEN,
+        secret: env.TWITTER_ACCESS_TOKEN_SECRET,
+    }
+
+    const requestData = {
+        url: "https://api.twitter.com/2/tweets",
+        method: 'POST',
+    }
+
+    const response = await fetch(requestData.url, {
+        method: 'POST',
+        headers: {
+            ...oauth.toHeader(oauth.authorize(requestData, oauthToken)),
+            'content-type': "application/json",
+        },
+        body: JSON.stringify({ text, media: { media_ids: mediaList } }),
+    })
+
+    return await response.json()
+}
